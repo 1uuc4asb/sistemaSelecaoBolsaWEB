64f179825ccb02d099f80f7c9421f8e1941acf62
Guilherme do Valle <guilhermedovalle@Mac-mini-de-Guilherme.local>
2019-05-20 15:50:12 -0300

    Finaliza create de inscrição

--- selecaoAPI/app/Http/Controllers/InscricaoSelecaoController.php
+++ selecaoAPI/app/Http/Controllers/InscricaoSelecaoController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers;
 
 use App\SelecoesCandidatos;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 use Validator;
 use Auth;
+use Response;
 
 class InscricaoSelecaoController extends Controller
 {
@@ -21,10 +23,6 @@ class InscricaoSelecaoController extends Controller
 
   public function show($id)
   {
-    /*
-        TODO
-            Pegar dados da seleção com id = $id. Colocar no data pra passar pra view.
-    */
     $data = [
       'id' => $id
     ];
@@ -36,17 +34,19 @@ class InscricaoSelecaoController extends Controller
   {
     $inscricao = $request->all();
     $user = Auth::user();
-    $array = ['selecao_id' => $inscricao['inscricao'], 'semestre_atual' => $inscricao['semestre'],
-      'CH_cumprida' => $inscricao['CH'], 'CR_atual' => $inscricao['CR'], 'candidato_id' => $user->id];
-    SelecoesCandidatos::create($array);
+
+    if ($this->validarFormulario($inscricao)) {
+      $array = ['selecao_id' => $inscricao['inscricao'], 'semestre_atual' => $inscricao['semestre'],
+        'CH_cumprida' => $inscricao['CH'], 'CR_atual' => $inscricao['CR'], 'candidato_id' => $user->id];
+      $create = SelecoesCandidatos::create($array);
+      if ($create) {
+        return Redirect::route('home');
+      }
+    }
   }
 
   public function validarFormulario($data)
   {
-    /*
-        TODO
-            Fazer validação dos campos passados pelo post no método acima.
-    */
     return Validator::make($data, ['CR' => 'required|numeric',
       'CH' => 'required|numeric',
       'semestre' => 'required|integer']);
--- selecaoAPI/app/SelecoesCandidatos.php
+++ selecaoAPI/app/SelecoesCandidatos.php
@@ -6,13 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class SelecoesCandidatos extends Model
 {
-    protected $table = 'selecoes_candidatos';
+  protected $table = 'selecoes_candidatos';
 
-    protected $primaryKey = ['selecao_id','candidato_id'];
+  protected $primaryKey = 'candidato_id';
 
-    protected $fillable = [
-        'selecao_id','candidato_id','CR_atual','CH_cumprida','semestre_atual'
-    ];
+  protected $fillable = [
+    'selecao_id', 'candidato_id', 'CR_atual', 'CH_cumprida', 'semestre_atual'
+  ];
 
 
 }
