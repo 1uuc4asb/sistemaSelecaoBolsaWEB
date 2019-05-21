4dc5e38e7da409e09d3678b6ae3f53385d1edc10
Gabriel Costa <gab_costa_2005@yahoo.com.br>
2019-05-21 15:04:14 -0300

    Update resultado.blade.php
    estilo na tabela de resultados.

--- selecaoAPI/resources/views/resultado.blade.php
+++ selecaoAPI/resources/views/resultado.blade.php
@@ -1,45 +1,60 @@
 @extends('layouts.app')
 
 @section('content')
-    @if(!$selecao->isFinished($selecao->id))
-        <h1>Resultado parcial da seleção {{$selecao->nome}}</h1>
-    @else
-        <h1>Resultado final da seleção {{$selecao->nome}}</h1>
-    @endif
-    <table>
-        <thead>
-        <tr>
-            <th>
-                Nome do candidato
-            </th>
-
-            <th>
-                Semestre atual
-            </th>
-            <th>
-                {{$selecao->parametro_de_comparacao}}
-            </th>
-        </tr>
-        </thead>
-        <tbody>
-        @foreach($inscricoes as $inscricao)
-            <tr>
-                <td>
-                    {{$inscricao->name}}
-                </td>
-                <td>
-                    {{$inscricao->semestre_atual}}
-                </td>
-                <td>
-                    @if($selecao->parametro_de_comparacao == 'CH')
-                        {{$inscricao->CH_cumprida}}
-                    @elseif($selecao->parametro_de_comparacao == 'CR')
-                        {{$inscricao->CR_atual}}
+ <div class="container">
+	<a href="{{ url('/home') }}">Home</a>
+	<div class="row justify-content-center">
+		<div class="col-md-12">
+			<div class="card">
+				<div class="card-body">
+					@if (session('status'))
+						<div class="alert alert-success" role="alert">
+							{{ session('status') }}
+						</div>
                     @endif
-                </td>
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
-
+					@if(!$selecao->isFinished($selecao->id))
+						<h3>Resultado parcial da seleção {{$selecao->nome}}</h3>
+					@else
+						<h3>Resultado final da seleção {{$selecao->nome}}</h3>
+					@endif
+					<table class="table">
+						<thead>
+							<tr>
+								<th scope="col">
+									Nome do candidato
+								</th>
+								<th  scope="col">
+									Semestre atual
+								</th>
+								<th  scope="col">
+									{{$selecao->parametro_de_comparacao}}
+								</th>
+							</tr>
+						</thead>
+						<tbody>
+							@foreach($inscricoes as $inscricao)
+							<tr>
+								<td>
+									{{$inscricao->name}}
+								</td>
+								<td>
+									{{$inscricao->semestre_atual}}
+								</td>
+								<td>
+									@if($selecao->parametro_de_comparacao == 'CH')
+										{{$inscricao->CH_cumprida}}
+									@elseif($selecao->parametro_de_comparacao == 'CR')
+										{{$inscricao->CR_atual}}
+									@elseif($selecao->parametro_de_comparacao == 'SEMESTRE')
+										{{$inscricao->semestre_atual}}
+									@endif
+								</td>
+							</tr>
+							@endforeach
+						</tbody>
+					</table>
+               </div>
+			</div>
+		</div>
+	</div>
 @endsection
