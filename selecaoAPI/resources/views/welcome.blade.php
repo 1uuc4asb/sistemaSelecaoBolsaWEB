b5e7056b3a24b7a64373c3fe4a436516484bb784
Guilherme do Valle <guilhermedovalle@Mac-mini-de-Guilherme.local>
2019-05-21 17:59:28 -0300

    Altera welcome

--- selecaoAPI/resources/views/welcome.blade.php
+++ selecaoAPI/resources/views/welcome.blade.php
@@ -1,96 +1,144 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Sistema de seleção de bolsa</title>
+    <title>Sistema de seleção de bolsa</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+    <!-- Styles -->
+    <style>
+        html, body {
+            background-color: #B3E5FC;
+            color: #636b6f;
+            font-family: 'Nunito', sans-serif;
+            font-weight: 200;
+            height: 100vh;
+            margin: 0;
+        }
 
-            .full-height {
-                height: 100vh;
-            }
+        .full-height {
+            height: 100vh;
+        }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+        .header {
+            text-align: center;
+            background-color:#03A9F4;
+            color: white;
+            height: 400px;
+            width: 100%;
+            position: fixed;
+        }
 
-            .position-ref {
-                position: relative;
-            }
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
 
-            .content {
-                text-align: center;
-            }
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
 
-            .title {
-                font-size: 84px;
-            }
+        .position-ref {
+            position: relative;
+        }
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+        .content {
+            text-align: center;
+        }
 
-                        @if (Route::has('register'))
-                        @endif
-                    @endauth
-                </div>
-            @endif
+        .title {
+            font-size: 84px;
+        }
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Sistema de seleção de bolsa
-                </div>
+        .links > a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 13px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
 
-                <div class="links">
-                    @guest
-                        Bem vindo ao melhor site de seleção de bolsas do mundo! Faça Login para visualizar as seleções.
-                    @else
-                        O melhor site de seleção de bolsas do mundo!
-                    @endguest
-                </div>
-            </div>
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+
+        button {
+            background-color: #7299ff;
+            color: white;
+            padding: 20px;
+            height: 100px;
+            width: 200px;
+        }
+    </style>
+</head>
+<body>
+<div class="position-ref full-height">
+    @if (Route::has('login'))
+        <div class="top-right links">
+            @auth
+                <a href="{{ url('/home') }}">Home</a>
+            @else
+                <a href="{{ route('login') }}">Login</a>
+
+                @if (Route::has('register'))
+                @endif
+            @endauth
         </div>
-    </body>
+    @endif
+
+    {{--<div class="content">
+        <div class="title m-b-md">
+            Sistema de seleção de bolsa
+        </div>--}}
+
+    <div class="links">
+        {{--@guest
+            Bem vindo ao melhor site de seleção de bolsas do mundo! Faça Login para visualizar as seleções.
+        @else
+            O melhor site de seleção de bolsas do mundo!
+        @endguest--}}
+    </div>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+
+    <div class="header">
+        <div class="content">
+
+
+            <h1 style="font-size: 50px; margin-top: 100px;">
+                iSelect - A solução para seleções de bolsas de estudo
+            </h1>
+            <h2>
+                A forma mais fácil e rápida para se candidatar ou oferecer oportunidades de bolsas de estudo!
+            </h2>
+            <h4>Feito por: Alexandre, Gabriel, Guilherme, Lucas e Wilton!</h4>
+            {{--<div align="row">
+                <button>
+                    Realizar login
+                </button>
+                <button>
+                    Não possuo conta, desejo me cadastrar
+                </button>
+            </div>--}}
+        </div>
+
+    </div>
+</div>
+</body>
 </html>
